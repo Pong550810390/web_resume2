a0cc02480fffc1ae1a9c704b42853b02de3264cd
nattapongsaethao <64143172@cmru.ac.th>
2024-08-10 17:52:02 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=2.0">
+    <title>Resume</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <?php include 'resume_data.php'; ?>
+    
+    <div class="container">
+    <aside class="sidebar">
+            <nav>
+                <ul>
+                    
+                    <li><a href="#information">Personal Information</a></li>
+                    <li><a href="#about">About Me</a></li>
+                    <li><a href="#experience">Activity</a></li>
+                    <li><a href="#education">Education</a></li>
+                    <li><a href="#skills">Skills</a></li>
+                </ul>
+            </nav>
+        </aside>
+    <main class="content">
+        <header>
+        <img src="image/1.jpg" alt="image" class="image">
+            <h1><?php echo $name; ?></h1>
+            
+        </header>
+        <section id="about">
+                <h2>About Me</h2>
+                <p><?php echo $about; ?></p>
+            </section>
+        <section id="information">
+        <section>
+            <h2>Personal Information</h2>
+            <p><?php echo "Name: ".$name; ?></p>
+            <p><?php echo "Age: ".$age; ?></p>
+            <p><?php echo "Sex: ".$Sex; ?></p>
+            <p><?php echo "Email: " . $email; ?></p>
+            <p><?php echo "Phone Number: ".$phone_number; ?></p>
+            <P><?php echo "Address: ".$address; ?></P>
+        </section>
+        <section id="experience">
+        <section>
+            <h2>Activity</h2>
+            <ul>
+                <?php foreach ($activity as $job) : ?>
+                    <li>
+                        
+                        <h3><?php echo $job['title']; ?> </h3>
+                        <P><?php echo $job['Organizer']; ?><p>
+                        <p><?php echo $job['date']; ?></p>
+                        
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </section>
+        <section id="education">
+        <section>
+
+            <h2>Education</h2>
+            <ul>
+                <?php foreach ($education as $edu) : ?>
+                    <div class="education-section">
+                    <img src="<?php echo $edu['image']; ?>" alt="Education Image" class="education-image">
+                    <div>
+                        <h3><?php echo $edu['degree']; ?></h3>
+                        <p><?php echo $edu['school']; ?></p>
+                        <p><?php echo $edu['date']; ?></p>
+                    </div>
+                </div>
+                <?php endforeach; ?>
+            </ul>
+        </section>
+        <section id="skills">
+                <h2>Skills</h2>
+                <ul>
+                    <?php foreach ($skills as $skill) : ?>
+                        <li><?php echo $skill; ?></li>
+                    <?php endforeach; ?>
+                </ul>
+            </section>
+    </main>
+    </div>
+</body>
+</html>
---
+++ resume_data.php
@@ -0,0 +1,52 @@
+<?php
+$name = "Nattapong saethao";
+$email = "nattapongsaethao.nan@gmail.com";
+$phone_number ="0636756021";
+$Sex = "male";
+$age ="22";
+$address = "40/1 Muen Dam Prakot Road, Chang Phueak Subdistrict, Mueang Chiang Mai District 50300";
+
+$activity = [
+    [
+        'title' => 'Training Java Applications with Spring boot Framework and Angular JS',
+        'Organizer' => 'Faculty of Science and Technology Chiang Mai Rajabhat University',
+        'date' => '2567'
+    ],
+    [
+        'title' => 'Training Mobile Application Development Training With Flutter',
+        'Organizer' => 'Faculty of Science and Technology Chiang Mai Rajabhat University',
+        'date' => '2567'
+    ],
+    [
+        'title' => 'Training in UX/UI design for 21st century applications',
+        'Organizer' => 'Faculty of Science and Technology Chiang Mai Rajabhat University',
+        'date' => '2566'
+    ]
+];
+
+$education = [
+    [   'image' => 'image/3.png',
+        'degree' => 'Bachelor of Science in Computer Science',
+        'school' => 'Chiang Mai Rajabhat University',
+        'date' => ' 2564 - current'
+    ],
+    [   
+        'image' => 'image/4.png',
+        'degree' => 'High School ',
+        'school' => 'Chiang Klang School "Prachapatthana"',
+        'date' => ' 2561 - 2564'
+    ]
+];
+$skills = [
+    'PHP',
+    'Dart',
+    'Java',
+    'HTML/CSS',
+    'MySQL',
+    'JavaScript',
+    'Flutter',
+    'Git'
+];
+$about = "I am Mr. Natthaphong Saethao, currently studying at Chiang Mai Rajabhat University, majoring in Computer Science. I am interested in programming to develop applications and websites. During my studies, I learned and practiced developing my programming skills to be able to use them more efficiently. I sincerely hope to be able to put my skills and knowledge to use working with a team of professionals. To increase work experience and develop further skills"
+
+?>
